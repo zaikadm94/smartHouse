4c502ac6556860a64e618fec90377e9c0f14cd78
zaikadm94 <zaikadm94@mail.ru>
2023-05-18 14:50:43 +0300

    add test

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,4 +1,17 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RadioTest {
+
+    @Test
+    public void nextRadioNumber() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioNumber(9);
+        radio.NextNumberStation();
+        int expected = 0;
+        int actual = radio.getCurrentRadioNumber();
+        Assertions.assertEquals(expected, actual);
+    }
 }
